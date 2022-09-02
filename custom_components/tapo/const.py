87557051949e1ac68b2d305b6798e53f486ae281
Andrea <petretiandrea@gmail.com>
2022-09-02 19:37:19 +0000

    bump version to 1.2.15

--- custom_components/tapo/const.py
+++ custom_components/tapo/const.py
@@ -8,7 +8,7 @@ from homeassistant.components.light import (
 
 NAME = "tapo"
 DOMAIN = "tapo"
-VERSION = "1.2.14"
+VERSION = "1.2.15"
 
 SUPPORTED_DEVICE_AS_SWITCH = ["p100", "p105", "p110", "p115"]
 SUPPORTED_DEVICE_AS_SWITCH_POWER_MONITOR = ["p110", "p115"]
