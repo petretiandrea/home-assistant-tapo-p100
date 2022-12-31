04c1b4ab82292814a44ecd79717e5e1cf5f61ed5
petretiandrea <petretiandrea@gmail.com>
2022-12-31 12:03:40 +0000

    bump version to 1.3.2

--- custom_components/tapo/const.py
+++ custom_components/tapo/const.py
@@ -4,7 +4,7 @@ from homeassistant.components.light import ColorMode
 
 NAME = "tapo"
 DOMAIN = "tapo"
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 
 SUPPORTED_DEVICE_AS_SWITCH = ["p100", "p105", "p110", "p115"]
 SUPPORTED_DEVICE_AS_SWITCH_POWER_MONITOR = ["p110", "p115"]
