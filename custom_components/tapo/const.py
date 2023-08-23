ea833834c3c3da7b2909eb5f50fd35f34b44d14c
petretiandrea <petretiandrea@gmail.com>
2023-08-23 11:32:38 +0000

    bump version to 2.6.1

--- custom_components/tapo/const.py
+++ custom_components/tapo/const.py
@@ -5,7 +5,7 @@ from plugp100.api.light_effect_preset import LightEffectPreset
 
 NAME = "tapo"
 DOMAIN = "tapo"
-VERSION = "2.6.0"
+VERSION = "2.6.1"
 
 SUPPORTED_HUB_DEVICE_MODEL = "h100"
 SUPPORTED_POWER_STRIP_DEVICE_MODEL = "p300"
