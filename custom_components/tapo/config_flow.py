f5ab37d159d034d280da9503d1076cb665d4bba7
petretiandrea <petretiandrea@gmail.com>
2024-03-05 21:25:16 +0100

    fix test after mac unique id on config entry

--- custom_components/tapo/config_flow.py
+++ custom_components/tapo/config_flow.py
@@ -336,7 +336,7 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             )
             return self.async_create_entry(title="", data={})
         return self.async_show_form(
-            step_id=STEP_INIT,
+            step_id="init",
             data_schema=step_options(self.config_entry),
         )
 
